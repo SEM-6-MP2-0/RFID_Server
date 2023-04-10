a29600b9ac3b899b1e8d4f87c2c9ac78991f6b52
SalmanAd01 <salmanadhikari415@gmail.com>
2023-04-10 20:41:35 +0530

    created the schema for students and faculty

---
+++ models/faculty.js
@@ -0,0 +1,66 @@
+const mongoose = require('mongoose');
+const schema = mongoose.Schema;
+const { encryptPassword, comparePassword } = require('../utils/bcrypt.utils');
+const { createAccessToken } = require('../utils/jwt.utils');
+
+const FacultySchema = new schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    isHod: {
+      type: Boolean,
+      default: false,
+    },
+    isClassIncharge: {
+      type: Boolean,
+      default: false,
+    },
+    isValid: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+FacultySchema.pre('save', async function (next) {
+  let mentor = this;
+  if (!mentor.isModified('password')) return next();
+  const hash = await encryptPassword(mentor.password);
+  mentor.password = hash;
+  return next();
+});
+
+FacultySchema.methods.comparePassword = async function (password) {
+  const user = this;
+  const isMatch = await comparePassword(password, user.password);
+  return isMatch;
+};
+
+FacultySchema.methods.generateAuthToken = function () {
+  const faculty = this;
+  const token = createAccessToken(faculty);
+  return {
+    token,
+    role: faculty.isHod
+      ? 'hod'
+      : faculty.isClassIncharge
+      ? 'classIncharge'
+      : 'faculty',
+  };
+};
+const Faculty = mongoose.model('faculty', FacultySchema);
+module.exports = Faculty;
---
+++ models/students.js
@@ -0,0 +1,64 @@
+const mongoose = require('mongoose');
+const { comparePassword } = require('../utils/bcrypt.utils');
+const { createAccessToken } = require('../utils/jwt.utils');
+const schema = mongoose.Schema;
+
+const StudentsSchema = new schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    prn: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    dateofjoining: {
+      type: String,
+      required: true,
+    },
+    attendance: [
+      {
+        subject: String,
+        attended: Boolean,
+        semester: String,
+        createdAt: {
+          type: Date,
+          default: Date.now,
+        },
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+StudentsSchema.methods.generateAuthToken = function () {
+  const student = {
+    _id: this._id,
+    name: this.name,
+    email: this.email,
+    prn: this.prn,
+  };
+  const token = createAccessToken(student);
+  return { token, role: 'student' };
+};
+
+StudentsSchema.methods.comparePassword = async function (password) {
+  const user = this;
+  const isMatch = await comparePassword(password, user.password);
+  return isMatch;
+};
+const Students = mongoose.model('Students', StudentsSchema);
+module.exports = Students;
